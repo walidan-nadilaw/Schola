32dea68095e252f8c62c9b6879996ea3965bb311
Aldi Pramudya <llenndesu@gmail.com>
2026-03-13 05:48:56 +0700

    feat: part 2, sqlalchemy

--- backend/main.py
+++ backend/main.py
@@ -1,94 +1,122 @@
-from typing import Optional
+from typing import Optional, List
 
-from fastapi import FastAPI, HTTPException, Path, status
+from fastapi import FastAPI, HTTPException, Path, status, Depends
 from pydantic import BaseModel
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
 
+from dotenv import load_dotenv
+import os
 
-# Base Pydantic Model
-class User(BaseModel):
-    name: str
-    website: str
-    age: int
-    role: str
+load_dotenv()
+app = FastAPI(title="Integration with SQL - Beginner Friendly")
 
+# Database Setup
 
-class UpdateUser(BaseModel):
-    name: Optional[str] = None
-    website: Optional[str] = None
-    age: Optional[int] = None
-    role: Optional[str] = None
+try:
+    DATABASE_URL = str(os.getenv("DATABASE_URL"))
+    engine = create_engine(DATABASE_URL)
 
+    print("SQLAlchemy engine created successfully.")
 
-app = FastAPI()
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();"))
+        print("Connection successful. PostgreSQL version:")
+        for row in result:
+            print(row[0])
 
-users = {
-    1: {"name": "Mike", "website": "www.google.com", "age": 28, "role": "Developer"}
-}
+except Exception as e:
+    print(f"An error occurred: {e}")
 
+SessionLocal = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
+Base = declarative_base()
+
+#Database Models
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+    role = Column(String(100), nullable=False)
+
+Base.metadata.create_all(engine)
+
+#Pydantic Models (dataclasses)
+class UserCreate(BaseModel):
+    name:str
+    email:str
+    role:str
+
+class UserResponse(BaseModel):
+    id:int
+    name:str
+    email:str
+    role:str
+
+    class Config:
+        from_attributes = True
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+get_db()
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Halo SQLAlchemy!"}
 
-
-# GET USERS
-@app.get("/users/{user_id}")
-def get_user(
-    user_id: int = Path(..., description="The ID you want to get", gt=0, lt=100),
-):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User does not exist in DB.")
-    return users[user_id]
-
-
-# Create Users
-@app.post("/users/{user_id}", status_code=status.HTTP_201_CREATED)
-def create_user(user_id: int, user: User):
-    if user_id in users:
-        raise HTTPException(status_code=400, detail="User already exists.")
-
-    users[user_id] = user.model_dump()
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id:int, db:Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found.")
     return user
 
-
-# Update a user
-@app.put("/users/{user_id}")
-def update_user(user_id: int, user: UpdateUser):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User does not exists.")
-
-    current_user = users[user_id]
-
-    if user.name is not None:
-        current_user["name"] = user.name
-    if user.website is not None:
-        current_user["website"] = user.website
-    if user.age is not None:
-        current_user["age"] = user.age
-    if user.role is not None:
-        current_user["role"] = user.role
-
-    return current_user
-
-
-# Delete a user
-@app.delete("/users/{user_id}")
-def delete_user(user_id: int):
-    if user_id not in users:
-        raise HTTPException(status_code=404, detail="User does not exists.")
-    deleted_user = users.pop(user_id)
-
-    return {"message": "User has been deleted.", "deleted_user": deleted_user}
-
-
-# Search for a user
-@app.get("/users/search/")
-def search_by_name(name: Optional[str] = None):
-    if not name:
-        return {"message": "Name parameter is required"}
+@app.post("/users/", response_model=UserResponse)
+def create_user(user: UserCreate, db:Session = Depends(get_db)):
+    if db.query(User).filter(User.email == user.email).first():
+        raise HTTPException(status_code=409, detail="User already exists.")
     
-    for user in users.values():
-        if user["name"] == name:
-            return user
-        
-    raise HTTPException(status_code=404, detail="User not found.")
+    # Create
+    new_user = User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+# update user
+@app.put("/users/{user_id}", response_model=UserResponse)
+def update_user(user_id:int, user:UserCreate, db:Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    
+    for field, value in user.model_dump().items():
+        setattr(db_user, field, value)
+
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+# delete user
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int, db:Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found.")
+    
+    db.delete(db_user)
+    db.commit()
+    return {"message": "User Deleted.", "deleted_user": db_user}
+
+# get all users
+@app.get("/users/", response_model=List[UserResponse])
+def get_all_users(db:Session = Depends(get_db)):
+    return db.query(User).all()
