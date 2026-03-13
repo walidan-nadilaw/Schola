f0bdbacf68fcfe6abc7403c76a576a33d1fef942
Aldi Pramudya <llenndesu@gmail.com>
2026-03-13 07:38:02 +0700

    feat: db.py

--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,33 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = ""
+from dotenv import load_dotenv
+import os
+
+try:
+    DATABASE_URL = str(os.getenv("DATABASE_URL"))
+    engine = create_engine(DATABASE_URL)
+
+    print("SQLAlchemy engine created successfully.")
+
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();"))
+        print("Connection successful. PostgreSQL version:")
+        for row in result:
+            print(row[0])
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
